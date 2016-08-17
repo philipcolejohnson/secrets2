fa6562ec52c77e03c8d05a9554c74e80dc6a3b9e
Hannah Squier <hannahsquier@gmail.com>
2016-08-17 10:16:28 -0700

    finish tests 4-5

--- spec/controllers/secrets_controller_spec.rb
+++ spec/controllers/secrets_controller_spec.rb
@@ -23,6 +23,11 @@ describe SecretsController do
 
         expect(response).to render_template :edit
       end
+
+      it "sets the right instance variables" do
+        get :edit, id: secret.id
+        expect(assigns(:secret)).to match(secret)
+      end
     end
 
     describe "POST #update" do
@@ -32,9 +37,23 @@ describe SecretsController do
 
         expect(secret.title).to eql("New Secret")
       end
+
+      it "doesn't update a secret with invalid attributes" do
+        put :update, id: secret.id, secret: attributes_for(:secret, title: nil)
+        secret.reload
+        expect(response).to render_template :edit
+      end
+
+      it "doesn't update a secret with invalid secret id" do
+        expect{
+          put :update, id: 123, secret: attributes_for(:secret, title: "New Secret")
+          }.to raise_error ActiveRecord::RecordNotFound
+      end
     end
 
     describe "DELETE #destroy" do
+      let(:secret2) { create(:secret) }
+
       it "deletes secret" do
         expect {
           delete :destroy, id: secret.id
@@ -45,6 +64,52 @@ describe SecretsController do
         delete :destroy, id: secret.id
 
         expect(response).to redirect_to secrets_path
+      end
+
+
+      it "doesn't allow you to delete a secret that isn't yours" do
+
+        expect {
+          delete :destroy, id: secret2.id
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+    end
+
+    describe "POST #create" do
+
+      it "will create a new secret with proper submission" do
+        expect{
+          post :create, secret: attributes_for(:secret)
+         }.to change(Secret, :count).by(1)
+      end
+
+      it "will redirec to to show page with proper submission" do
+        post :create, secret: attributes_for(:secret)
+        expect(response).to redirect_to secret_path(assigns(:secret))
+      end
+
+      it "will not create a new secret with improper submission" do
+        expect{
+          post :create, secret: attributes_for(:secret, title: nil)
+        }.to_not change(Secret, :count)
+      end
+
+
+
+      it "will render new page with improper submission" do
+        post :create, secret: attributes_for(:secret, title: nil)
+        expect(response).to render_template :new
+      end
+
+      it "sets notice flash message when successfully created" do
+        post :create, secret: attributes_for(:secret)
+        expect(flash[:notice]).to_not eq(nil)
+      end
+
+        it "no flash message when not successfully created" do
+        post :create, secret: attributes_for(:secret, title: nil)
+        expect(flash[:notice]).to eq(nil)
       end
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe UsersController do
+  let(:user) { create(:user) }
   describe "POST #create" do
 
     it "will create a new user with proper submission" do
@@ -28,5 +29,69 @@ describe UsersController do
     end
   end
 
-  
+
+  context "user is signed in" do
+    before do
+      session[:user_id] = user.id
+    end
+
+    describe "GET #edit" do
+      it "renders an edit page" do
+        get :edit, id: user.id
+
+        expect(response).to render_template :edit
+      end
+
+      it "sets the right instance variables" do
+        get :edit, id: user.id
+        expect(assigns(:user)).to match(user)
+      end
+    end
+
+    describe "PUT #update" do
+      it "updates a user with valid attributes" do
+        patch :update, id: user.id, user: attributes_for(:user, name: "New Name")
+        user.reload
+
+        expect(user.name).to eql("New Name")
+      end
+
+      it "doesn't update a user with invalid attributes" do
+        put :update, id: user.id, user: attributes_for(:user, name: "no")
+        user.reload
+        expect(user.name).to eql("Foo")
+      end
+
+      it "doesn't update a user with invalid user id" do
+        expect{
+          put :update, id: 123, user: attributes_for(:user, name: "New User")
+          }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
+    describe "DELETE #destroy" do
+      let(:user2) { create(:user) }
+
+      it "deletes user" do
+        expect {
+          delete :destroy, id: user.id
+        }.to change(User, :count).by(-1)
+      end
+
+      it "redirects to index" do
+        delete :destroy, id: user.id
+        expect(response).to redirect_to users_path
+      end
+
+
+      it "doesn't allow you to delete a user that isn't yours" do
+
+        expect{
+          delete :destroy, id: user2.id
+        }.to_not change(User, :count)
+      end
+
+
+    end
+  end
 end
